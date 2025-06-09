940e59fcd7bb4d216c1f66552ec3ec212acd5c63
Jackyno <jackynojeanbaptiste@gmail.com>
2025-06-09 17:36:58 -0400

    phase-1-javascript-functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+// function declaration
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// function expression
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+// function returning a function (function closure)
+function wrapAdjective(symbol = "*") {
+  return function(adjective = "special") {
+    return `You are ${symbol}${adjective}${symbol}!`;
+  };
+}
+
